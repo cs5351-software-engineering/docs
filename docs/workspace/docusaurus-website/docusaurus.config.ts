55d6daafbf25568e454b6e39cdaefd446b9d146b
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-10-09 19:53:11 +0800

    fix: baseUrl to docs

--- docs/workspace/docusaurus-website/docusaurus.config.ts
+++ docs/workspace/docusaurus-website/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://cs5351-software-engineering.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cs5351-codebase-analysis/',
+  baseUrl: '/docs/',
   // baseUrl : '/',
 
   // GitHub pages deployment config.
